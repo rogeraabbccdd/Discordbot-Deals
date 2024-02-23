697195b62c8e18cc8cf5a3ac114de00639db660c
rogeraabbccdd <rogeraabbccdd@gmail.com>
2024-02-23 09:52:23 +0800

    add exrate update logs

--- funcs/exRateUpdate.js
+++ funcs/exRateUpdate.js
@@ -4,8 +4,10 @@ module.exports = async () => {
   let result = 30
 
   try {
+    console.log('Updating exRate...')
     const { data } = await axios.get('https://tw.rter.info/capi.php')
     result = Math.round(data.USDTWD.Exrate * 100) / 100
+    console.log('Updated exRate: ' + result)
   } catch (error) {
     console.log('exRateUpdate Error')
     if (process.env.ERROR === 'true') console.log(error)
