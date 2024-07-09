05764b976e4dc530390c2ef002c3169ecf2d75d2
rogeraabbccdd <rogeraabbccdd@gmail.com>
2024-07-09 10:07:45 +0800

    Update help command

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,8 @@ const { SlashCommandBuilder } = require('discord.js')
 
 const helpReply =
 ':desktop:  機器人指令\n' +
-'• `/itad 遊戲名稱` - 查詢遊戲資訊\n' +
+'• `/itad <遊戲名稱>` - 查詢遊戲資訊\n' +
+'• `/itadid <SteamID>` - 查詢遊戲資訊\n' +
 '\n:link:  相關連結\n' +
 '• 巴哈文章: https://forum.gamer.com.tw/C.php?bsn=60599&snA=27046\n' +
 '• 邀請連結: https://discordapp.com/oauth2/authorize?client_id=634902541687324702&scope=bot&permissions=28832\n' +
