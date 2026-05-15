5ed294002f85d467a71cace0cadd1387d0d401d6
Kento <rogeraabbccdd@gmail.com>
2026-05-15 12:37:10 +0800

    Fix SteamDB

--- funcs/fetchSteamDB.js
+++ funcs/fetchSteamDB.js
@@ -4,14 +4,20 @@ module.exports = async (id) => {
   let result = {}
 
   try {
-    const { data } = await axios.get('https://steamdb.info/api/ExtensionAppPrice/', {
+    const { data } = await axios.get('https://extension.steamdb.info/api/ExtensionAppPrice/', {
       params: {
         appid: id,
         currency: 'TWD'
       },
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-        'X-Requested-With': 'SteamDB'
+        Accept: 'application/json',
+        'Dnt': '1',
+        Origin: 'chrome-extension://kdbmhfkmnlmbkgbabkdealhhbfhlmmon',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'cross-site',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36',
+        'X-Requested-With': 'SteamDB',
       }
     })
     result = data
